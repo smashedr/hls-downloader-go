57699b7007fbe9b34a1d31b042431946c4bb75cd
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-01-27 20:50:24 -0800

    Add Linux Multi-Architecture Build (#10)

--- assets/build-linux.sh
+++ assets/build-linux.sh
@@ -3,61 +3,77 @@
 set -e
 
 APP_NAME="org.cssnr.hls.downloader"
-
 PKG_NAME="hls-downloader-client"
 SOURCE="dist"
 VERSION="0.0.1"
 
-echo "Debug - pwd: $(pwd)"
-echo "::group::Debug - SOURCE: ${SOURCE}"
+echo "CMD: ${0}"
+echo "PWD: ${PWD}"
+echo "::group::SOURCE: ${SOURCE}"
 tree "${SOURCE}" || ls -lAhR "${SOURCE}"
 echo "::endgroup::"
 
 if [ "${GITHUB_EVENT_NAME}" == "release" ];then
     VERSION="${GITHUB_REF_NAME}"
 fi
-echo "Building version: ${VERSION}"
+echo "VERSION: ${VERSION}"
 
-PACKAGE="${PKG_NAME}_${VERSION}"
+mkdir -p out
 
-echo "SOURCE: ${SOURCE}"
-ls -lah "${SOURCE}"
-echo "dest: ${PACKAGE}"
+declare -A ARCHS=(
+    [client_linux_amd64_v1]="amd64"
+    [client_linux_386_sse2]="i386"
+    [client_linux_arm64_v8.0]="arm64"
+)
 
-chrome="${PACKAGE}/etc/opt/chrome/native-messaging-hosts"
-chromium="${PACKAGE}/etc/chromium/native-messaging-hosts"
-firefox="${PACKAGE}/usr/lib/mozilla/native-messaging-hosts"
+#for ARCH_INFO in "amd64:client_linux_amd64_v1" "i386:client_linux_386_sse2" "arm64:client_linux_arm64_v8.0"; do
+#    ARCH="${ARCH_INFO%%:*}"
+#    CLIENT_DIR="${ARCH_INFO#*:}"
+for CLIENT_DIR in "${!ARCHS[@]}"; do
+    echo "----- ${CLIENT_DIR} -----"
+    ARCH="${ARCHS[$CLIENT_DIR]}"
+    echo "ARCH: ${ARCH}"
 
-mkdir -p "${PACKAGE}/DEBIAN"
-mkdir -p "${PACKAGE}/opt/${APP_NAME}"
-mkdir -p "${chrome}"
-mkdir -p "${chromium}"
-mkdir -p "${firefox}"
+    PACKAGE="${PKG_NAME}_${VERSION}_${ARCH}"
+    echo "PACKAGE: ${PACKAGE}"
 
-cp -f "dist/client/client_linux_amd64_v1/client" "${PACKAGE}/opt/${APP_NAME}/client"
-chmod +x "${PACKAGE}/opt/${APP_NAME}/client"
-touch "${PACKAGE}/opt/${APP_NAME}/log.txt" "${PACKAGE}/opt/${APP_NAME}/log-1.txt"
-chmod g+w "${PACKAGE}/opt/${APP_NAME}/log.txt" "${PACKAGE}/opt/${APP_NAME}/log-1.txt"
+    chrome="${PACKAGE}/etc/opt/chrome/native-messaging-hosts"
+    chromium="${PACKAGE}/etc/chromium/native-messaging-hosts"
+    firefox="${PACKAGE}/usr/lib/mozilla/native-messaging-hosts"
 
-cp -f "${SOURCE}/manifest-chrome.json" "${chrome}/${APP_NAME}.json"
-cp -f "${SOURCE}/manifest-chrome.json" "${chromium}/${APP_NAME}.json"
-cp -f "${SOURCE}/manifest-firefox.json" "${firefox}/${APP_NAME}.json"
+    mkdir -p "${PACKAGE}/DEBIAN"
+    mkdir -p "${PACKAGE}/opt/${APP_NAME}"
+    mkdir -p "${chrome}"
+    mkdir -p "${chromium}"
+    mkdir -p "${firefox}"
+
+    cp -f "dist/client/${CLIENT_DIR}/client" "${PACKAGE}/opt/${APP_NAME}/client"
+    chmod +x "${PACKAGE}/opt/${APP_NAME}/client"
+    touch "${PACKAGE}/opt/${APP_NAME}/log.txt" "${PACKAGE}/opt/${APP_NAME}/log-1.txt"
+    chmod g+w "${PACKAGE}/opt/${APP_NAME}/log.txt" "${PACKAGE}/opt/${APP_NAME}/log-1.txt"
+
+    cp -f "${SOURCE}/manifest-chrome.json" "${chrome}/${APP_NAME}.json"
+    cp -f "${SOURCE}/manifest-chrome.json" "${chromium}/${APP_NAME}.json"
+    cp -f "${SOURCE}/manifest-firefox.json" "${firefox}/${APP_NAME}.json"
 
 cat <<-EOF > "${PACKAGE}/DEBIAN/control"
 Package: ${PKG_NAME}
 Version: ${VERSION}
 Section: base
 Priority: optional
-Architecture: all
+Architecture: ${ARCH}
 Maintainer: CSSNR
 Description: HLS Video Downloader Native Client
 EOF
 
-echo "Debian: ${PACKAGE}/DEBIAN/control"
-cat "${PACKAGE}/DEBIAN/control"
+    echo "::group::cat ${PACKAGE}/DEBIAN/control"
+    cat -n "${PACKAGE}/DEBIAN/control"
+    echo "::endgroup::"
 
-echo "Building: ${PACKAGE}"
-dpkg-deb --build "${PACKAGE}"
+    echo "Building: ${PACKAGE}"
+    dpkg-deb --build "${PACKAGE}"
+    echo "Moving: out/linux-installer-${ARCH}.deb"
+    mv "${PACKAGE}.deb" "out/linux-installer-${ARCH}.deb"
+done
 
-mkdir out
-mv "${PACKAGE}.deb" "out/linux-installer.deb"
+echo "All Done."
